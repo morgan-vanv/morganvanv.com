8991d7d02ade7aecfaf26e2ebe9e86d45b644726
Morgan Van Valkenburgh <47067292+morgan-vanv@users.noreply.github.com>
2025-07-27 14:14:55 -0400

    Add landing page component and update app layout
    Introduces a new LandingPage component with associated HTML, SCSS, and test files. Updates app.html to use the new landing page, adds a background image, and applies new global and component styles. Also updates package dependencies to include @angular/material and makes minor adjustments to index.html for improved layout and font handling.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LandingPage} from './landing-page/landing-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingPage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/landing-page/landing-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [],
+  templateUrl: './landing-page.html',
+  styleUrl: './landing-page.scss'
+})
+export class LandingPage {
+
+}
