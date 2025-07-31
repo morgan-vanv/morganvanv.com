cdb63d8eba6d73560d1c803daac86ab28601d353
Morgan Van Valkenburgh <47067292+morgan-vanv@users.noreply.github.com>
2025-07-31 16:48:58 -0400

    Add social media icons and styling to home page
    Introduced Discord, Email, and GitHub SVG icons and a new social-media section to the landing page. Updated SCSS to style the icons with hover effects and tooltips. Moved favicon to /icons and updated index.html reference. Minor layout and style adjustments for improved appearance.

--- src/app/landing-page/home-page.component.ts
+++ src/app/landing-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { BasePageComponent} from '../../shared/base-page/base-page.component';
+import { BasePageComponent } from '../../shared/base-page/base-page.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
   standalone: true,
-  imports: [BasePageComponent]
+  imports: [BasePageComponent, MatIconModule]
 })
 export class HomePageComponent {
 
