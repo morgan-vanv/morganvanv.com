a0217f537e1234c3630f9ff7ee7d528c6d33895f
Morgan Van Valkenburgh <47067292+morgan-vanv@users.noreply.github.com>
2026-06-07 16:27:15 -0400

    Addressing another round of feedback on the PR
    thanks copilot!!!! i love feedback! keep it coming!!

--- src/app/social-page/social-page.component.spec.ts
+++ src/app/social-page/social-page.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { SocialPageComponent } from './social-page.component';
 
 describe('SocialPageComponent', () => {
@@ -7,7 +8,8 @@ describe('SocialPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SocialPageComponent]
+      imports: [SocialPageComponent],
+      providers: [provideRouter([])]
     })
     .compileComponents();
     
