be54d3bb004afad6b03de3840ee0340f4e856051
Morgan Van Valkenburgh <47067292+morgan-vanv@users.noreply.github.com>
2026-06-08 18:11:23 -0400

    dirty dan update

--- src/app/social-page/social-page.component.ts
+++ src/app/social-page/social-page.component.ts
@@ -14,6 +14,7 @@ const SUBMITTED_PHOTOS: Omit<PhotoSpot, 'id'>[] = [
   { imageUrl: 'images/community-wall/img-8938.webp', caption: 'a beast or demon of some sort' },
   { imageUrl: 'images/community-wall/img-20250303.webp', caption: 'Richard & Bernie <3' },
   { imageUrl: 'images/community-wall/gup.webp', caption: 'gup' },
+  { imageUrl: 'images/community-wall/dirty_dan.webp', caption: 'Dirty Dan' },
 ];
 
 @Component({
